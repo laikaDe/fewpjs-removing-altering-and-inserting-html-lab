9a65e46b4d34908cf8083305d0575da0829e8b19
Laika Deunert <laikadeunert@MacBook-Air.local>
2021-12-14 23:38:41 -0800

    commit tree 4eb38ca2ebb3b41921bf1b08af7cd923e6bec7f4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+let node = document.getElementById("main");
+if (node.parentNode) {
+    node.parentNode.removeChild(node);
+  }
+
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+
+newHeader.innerHTML = "YOUR - NAME is the champion";
